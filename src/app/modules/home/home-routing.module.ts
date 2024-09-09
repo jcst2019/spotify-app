c0493fe073a39683e0b990d021c3aa061f54d32b
jcst2019 <juansolar_4@hotmail.com>
2024-09-09 06:31:54 -0500

    RouterOutlet

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,12 +1,25 @@
+import { TracksModule } from './../tracks/tracks.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
-// import { HomePageComponent } from './pages/home-page/home-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
+  // {
+  //   path:'', // http://localhost:4200/
+  //   component:HomePageComponent
+  // }
   {
-    path:'', // http://localhost:4200/
-    component:SideBarComponent
+    path:'tracks', 
+    loadChildren:()=>import('@modules/tracks/tracks.module').then(m=>m.TracksModule)
+  },
+  {
+    path:'favorities', 
+    loadChildren:()=>import('@modules/favorites/favorites.module').then(m=>m.FavoritesModule)
+  },
+  {
+    path:'history', 
+    loadChildren:()=>import('@modules/history/history.module').then(m=>m.HistoryModule)
   }
 ];
 
