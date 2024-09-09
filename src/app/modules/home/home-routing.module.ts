74f316e99339f90cd92a6c04c125e9f82428057c
jcst2019 <juansolar_4@hotmail.com>
2024-09-09 05:16:27 -0500

    cinfiguracion final paths

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+// import { HomePageComponent } from './pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
     path:'', // http://localhost:4200/
-    component:HomePageComponent
+    component:SideBarComponent
   }
 ];
 
