315573a91a59d46ffad4434a282ab455a34577f5
jcst2019 <juansolar_4@hotmail.com>
2024-09-10 22:39:03 -0500

    usando tracks.json

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,22 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from '../../../../data/tracks.json'
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrl: './tracks-page.component.css'
 })
-export class TracksPageComponent {
+export class TracksPageComponent implements OnInit{
 
-  mockTracksList=[
-    {
-      name:'BEBE1(REOFICIAL)'
-    },
-    {
-      name:'BEBE2(REOFICIAL)'
-    },
-    {
-      name:'BEBE3(REOFICIAL)'
-    }
-  ];
+ 
+  // mockTracksList=[
+  //   {
+  //     name:'BEBE1(REOFICIAL)'
+  //   },
+  //   {
+  //     name:'BEBE2(REOFICIAL)'
+  //   },
+  //   {
+  //     name:'BEBE3(REOFICIAL)'
+  //   }
+  // ];
+
+  mockTracksList: Array<TrackModel> = [];
+
+  ngOnInit(): void {
+  //  console.log({data});
+  //  const data: any = (dataRaw as any).default;
+   const {data}: any = (dataRaw as any).default;
+   console.log({data});
+   this.mockTracksList=data;
+   
+  }
+
 
 }
