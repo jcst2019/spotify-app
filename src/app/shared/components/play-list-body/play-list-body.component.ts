541ff99764802c01e02c37dc16db0b3f842d0a9a
jcst2019 <juansolar_4@hotmail.com>
2024-09-11 12:38:23 -0500

    contexto(ng-template)

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import * as dataRaw from '../../../data/tracks.json'
 
 @Component({
   selector: 'app-play-list-body',
   templateUrl: './play-list-body.component.html',
   styleUrl: './play-list-body.component.css'
 })
-export class PlayListBodyComponent {
+export class PlayListBodyComponent implements OnInit{
 
+  tracks: Array<TrackModel> = [];
+
+  ngOnInit(): void {
+    const {data}: any = (dataRaw as any).default;
+    console.log({data});
+    this.tracks=data;
+  }
 }
--- src/app/shared/components/play-list-header/play-list-header.component.ts
+++ src/app/shared/components/play-list-header/play-list-header.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from '../../../data/tracks.json'
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-play-list-header',
   templateUrl: './play-list-header.component.html',
   styleUrl: './play-list-header.component.css'
 })
-export class PlayListHeaderComponent {
+export class PlayListHeaderComponent  {
+
+  
 
 }
