b8e60e6c42b4697f83fed392bbb0f8f8a68a9153
jcst2019 <juansolar_4@hotmail.com>
2024-09-10 22:59:38 -0500

    mostrando las canciones

--- src/app/shared/components/card-player/card-player.component.ts
+++ src/app/shared/components/card-player/card-player.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-card-player',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './card-player.component.css'
 })
 export class CardPlayerComponent {
+
+  @Input() mode: 'small' | 'big' = 'small'
+  @Input() track: TrackModel = { _id: 0, name: '', album: '', url: '', cover: '' };
+
 
 }
